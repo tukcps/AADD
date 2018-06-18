4f8cf66877d62129303de4accae96793975da700
Carna Radojicic <radojicic@bau49-inf-ag-grimm-ipv4-0022.informatik.uni-kl.de>
2018-06-18 12:01:24 +0200

    no message

--- examples/waterlevel_monitor/cmake-files/FindAADDLib.cmake
+++ examples/waterlevel_monitor/cmake-files/FindAADDLib.cmake
@@ -22,7 +22,7 @@
 # See the License for more information.
 #=============================================================================
 
-find_path(AADDLIB_INCLUDE_DIR NAMES AADD.h HINTS $ENV{AADDLIB_BASE} $ENV{AADDLIB}
+find_path(AADDLIB_INCLUDE_DIR NAMES aadd.h HINTS $ENV{AADDLIB_BASE} $ENV{AADDLIB}
           PATH_SUFFIXES include
           DOC "The AADDlib include directory"
 )
@@ -32,7 +32,7 @@ find_library(AADDLIB_LIBRARY NAMES aadd HINTS $ENV{AADDLIB_BASE} $ENV{AADDLIB}
           DOC "The AADDlib library"
 )
 
-# handle the QUIETLY and REQUIRED arguments and set XAAFLIB_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set AADDLIB_FOUND to TRUE if 
 # all listed variables are TRUE
 include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(AADDLIB
